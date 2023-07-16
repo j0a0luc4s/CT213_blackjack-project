dc37821d613964f2171fcdd91ad670d74a9c2b9f
Samir Nunes <samir.silva12342@gmail.com>
2023-07-16 15:13:10 -0300

    add keras import and requirements.yml for windows

--- dqn_agent.py
+++ dqn_agent.py
@@ -1,7 +1,8 @@
 from collections import deque
 import numpy as np
 import random
-from tensorflow.keras import models, layers, optimizers, activations, losses
+from keras import models, layers, optimizers, activations, losses
+#from tensorflow.keras import models, layers, optimizers, activations, losses
 
 class DQNAgent:
     """
